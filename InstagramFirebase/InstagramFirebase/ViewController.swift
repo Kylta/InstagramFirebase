520ff94c0ec3ea37404ae67c67207fd3504494de
Kylta <Christophe.bugnon02@gmail.com>
2018-09-08 10:23:21 +0200

    Creating a Registration Screen

--- InstagramFirebase/InstagramFirebase/ViewController.swift
+++ InstagramFirebase/InstagramFirebase/ViewController.swift
@@ -7,13 +7,75 @@
 //
 
 import UIKit
+import LBTAComponents
 
 class ViewController: UIViewController {
 
+    let plusPhotoButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setImage(#imageLiteral(resourceName: "plus_photo").withRenderingMode(.alwaysOriginal), for: .normal)
+        return button
+    }()
+    
+    let emailTextField: UITextField = {
+        let tf = UITextField()
+        tf.placeholder = "Email"
+        tf.backgroundColor = UIColor(white: 0, alpha: 0.03)
+        tf.borderStyle = .roundedRect
+        tf.font = UIFont.systemFont(ofSize: 14)
+        return tf
+    }()
+    
+    let userNameTextField: UITextField = {
+        let tf = UITextField()
+        tf.placeholder = "Username"
+        tf.backgroundColor = UIColor(white: 0, alpha: 0.03)
+        tf.borderStyle = .roundedRect
+        tf.font = UIFont.systemFont(ofSize: 14)
+        return tf
+    }()
+    
+    let passwordTextField: UITextField = {
+        let tf = UITextField()
+        tf.placeholder = "Password"
+        tf.backgroundColor = UIColor(white: 0, alpha: 0.03)
+        tf.borderStyle = .roundedRect
+        tf.font = UIFont.systemFont(ofSize: 14)
+        tf.isSecureTextEntry = true
+        return tf
+    }()
+    
+    let signUpButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Sign Up", for: .normal)
+        button.backgroundColor = UIColor.init(r: 149, g: 204, b: 244)
+        button.layer.cornerRadius = 5
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 14)
+        button.setTitleColor(.white, for: .normal)
+        return button
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        setupViews()
         
     }
+    
+    fileprivate func setupViews() {
+        
+        let stackFieldViews = UIStackView(arrangedSubviews: [emailTextField, userNameTextField, passwordTextField, signUpButton])
+        stackFieldViews.distribution = .fillEqually
+        stackFieldViews.axis = .vertical
+        stackFieldViews.spacing = 10
+        
+        [plusPhotoButton, stackFieldViews].forEach({view.addSubview($0)})
+        
+        plusPhotoButton.anchor(view.safeAreaLayoutGuide.topAnchor, left: nil, bottom: nil, right: nil, topConstant: 40, leftConstant: 0, bottomConstant: 0, rightConstant: 0, widthConstant: 140, heightConstant: 140)
+        plusPhotoButton.anchorCenterXToSuperview()
+        
+        stackFieldViews.anchor(plusPhotoButton.bottomAnchor, left: view.leftAnchor, bottom: nil, right: view.rightAnchor, topConstant: 40, leftConstant: 40, bottomConstant: 0, rightConstant: 40, widthConstant: 0, heightConstant: 200)
+    }
+    
 }
 
