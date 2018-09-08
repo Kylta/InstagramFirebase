db5f301d4e0ee75275c0e9e8bf31f1e0d22d8030
Kylta <Christophe.bugnon02@gmail.com>
2018-09-08 16:52:41 +0200

    User Profile Picture

--- InstagramFirebase/InstagramFirebase/ViewController.swift
+++ InstagramFirebase/InstagramFirebase/ViewController.swift
@@ -10,11 +10,10 @@ import UIKit
 import LBTAComponents
 import Firebase
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     let plusPhotoButton: UIButton = {
-        let button = UIButton(type: .system)
-        button.setImage(#imageLiteral(resourceName: "plus_photo").withRenderingMode(.alwaysOriginal), for: .normal)
+        let button = UIButton.systemButton(title: nil, image: #imageLiteral(resourceName: "plus_photo").withRenderingMode(.alwaysOriginal), titleColor: nil, font: nil, target: self, selector: #selector(handlePlusPhoto))
         return button
     }()
     
@@ -61,7 +60,29 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
         setupViews()
+    }
+    
+    @objc func handlePlusPhoto() {
+        let imagePickerController = UIImagePickerController()
+        imagePickerController.delegate = self
+        imagePickerController.allowsEditing = true
         
+        present(imagePickerController, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        if let editedImage = info["UIImagePickerControllerEditedImage"] as? UIImage {
+            plusPhotoButton.setImage(editedImage.withRenderingMode(.alwaysOriginal), for: .normal)
+        } else if let originalImage = info["UIImagePickerControllerOriginalImage"] as? UIImage {
+            plusPhotoButton.setImage(originalImage.withRenderingMode(.alwaysOriginal), for: .normal)
+        }
+        
+        plusPhotoButton.layer.cornerRadius = plusPhotoButton.frame.width/2
+        plusPhotoButton.layer.masksToBounds = true
+        plusPhotoButton.layer.borderWidth = 1
+        plusPhotoButton.layer.borderColor = UIColor.black.cgColor
+        
+        dismiss(animated: true, completion: nil)
     }
     
     @objc func handleTextInputChange() {
@@ -84,6 +105,39 @@ class ViewController: UIViewController {
                 print("Failed to register user:", err)
             }
             print("Successfully created user:", user?.user.uid ?? "")
+            
+            guard let image = self.plusPhotoButton.imageView?.image else { return }
+            guard let uploadData = UIImageJPEGRepresentation(image, 0.3) else { return }
+            
+            let filename = UUID().uuidString
+            let storageRef = Storage.storage().reference().child("profile_images").child(filename)
+            storageRef.putData(uploadData, metadata: nil, completion: { (metadata, err) in
+                if let err = err {
+                    print("Failed to upload profile image:", err)
+                    return
+                }
+                storageRef.downloadURL(completion: { (downloadUrl, err) in
+                    if let err = err {
+                        print("Failed to get url profile image", err)
+                    }
+                    guard let profileImageUrl = downloadUrl?.absoluteString else { return }
+                    
+                    print("Successfully uploaded profile image:", profileImageUrl)
+                    
+                    guard let uid = user?.user.uid else { return }
+                    
+                    let dictionaryValues = ["username":username, "profileImageUrl":profileImageUrl]
+                    let values = [uid:dictionaryValues]
+                    Database.database().reference().child("users").updateChildValues(values, withCompletionBlock: { (err, ref) in
+                        if let err = err {
+                            print("Failed to save user into database:", err)
+                            return
+                        }
+                        print("Successfully saved user into database")
+                    })
+                    
+                })
+            })
         }
     }
     
