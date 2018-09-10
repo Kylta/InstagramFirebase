51688eaca4b60b011fc9a8af514474820f6d3480
Kylta <Christophe.bugnon02@gmail.com>
2018-09-10 08:40:46 +0200

    User Photo Selection

--- InstagramFirebase/InstagramFirebase/AddPhoto/PhotoSelectorController.swift
+++ InstagramFirebase/InstagramFirebase/AddPhoto/PhotoSelectorController.swift
@@ -14,38 +14,64 @@ class PhotoSelectorController: UICollectionViewController, UICollectionViewDeleg
     let cellId = "cellId"
     let headerId = "headerId"
     var images = [UIImage]()
+    var assets = [PHAsset]()
+    var selectedImage: UIImage?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         collectionView?.register(PhotoSelectorCell.self, forCellWithReuseIdentifier: cellId)
-        collectionView?.register(UICollectionViewCell.self, forSupplementaryViewOfKind: UICollectionElementKindSectionHeader, withReuseIdentifier: headerId)
+        collectionView?.register(PhotoSelectorHeader.self, forSupplementaryViewOfKind: UICollectionElementKindSectionHeader, withReuseIdentifier: headerId)
         
         setupNavigationButtons()
         
         fetchPhotos()
     }
     
-    fileprivate func fetchPhotos() {
+    override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        selectedImage = images[indexPath.item]
+        self.collectionView?.reloadData()
+    }
+    
+    private func assetsFetchOptions() -> PHFetchOptions {
         let fetchOptions = PHFetchOptions()
-        fetchOptions.fetchLimit = 10
+//        fetchOptions.fetchLimit = 30
+        print(fetchOptions.fetchLimit)
         let sortDescriptors = NSSortDescriptor(key: "creationDate", ascending: false)
         fetchOptions.sortDescriptors = [sortDescriptors]
-        let allPhotos = PHAsset.fetchAssets(with: .image, options: fetchOptions)
+        return fetchOptions
+    }
+    
+    fileprivate func fetchPhotos() {
+        let allPhotos = PHAsset.fetchAssets(with: .image, options: assetsFetchOptions())
         
-        allPhotos.enumerateObjects { (asset, count, stop) in
-            let imageManager = PHImageManager.default()
-            let targetSize = CGSize(width: 350, height: 350)
-            let options = PHImageRequestOptions()
-            options.isSynchronous = true
-            
-            imageManager.requestImage(for: asset, targetSize: targetSize, contentMode: .aspectFit, options: nil, resultHandler: { (image, info) in
-                guard let image = image else { return }
-                self.images.append(image)
-                print(self.images)
-            })
-            count == (allPhotos.count - 1) ? self.collectionView?.reloadData() : nil
+        DispatchQueue.global(qos: .background).async {
+            allPhotos.enumerateObjects { (asset, count, stop) in
+                let imageManager = PHImageManager.default()
+                let targetSize = CGSize(width: 200, height: 200)
+                let options = PHImageRequestOptions()
+                options.isSynchronous = true
+                
+                imageManager.requestImage(for: asset, targetSize: targetSize, contentMode: .aspectFit, options: options, resultHandler: { (image, info) in
+                    if let image = image {
+                        self.images.append(image)
+                        self.assets.append(asset)
+                        //                    if self.selectedImage == nil {
+                        //                        self.selectedImage = image
+                        //                    }
+                        self.selectedImage == nil ? self.selectedImage = image : nil
+                    }
+                })
+//                if count == (allPhotos.count - 1) {
+//                    DispatchQueue.main.sync {
+//                        self.collectionView?.reloadData()
+//                    }
+//                }
+                
+                count == (allPhotos.count-1) ? DispatchQueue.main.sync {self.collectionView?.reloadData()} : nil
+            }
         }
+        
     }
     
     override var prefersStatusBarHidden: Bool {
@@ -57,10 +83,21 @@ class PhotoSelectorController: UICollectionViewController, UICollectionViewDeleg
     }
     
     override func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
-        let header = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: headerId, for: indexPath)
+        let header = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: headerId, for: indexPath) as! PhotoSelectorHeader
+
+//        header.photoImageView.image = selectedImage
         
-        header.backgroundColor = .blue
-        
+        if let selectedImage = selectedImage {
+            if let index = self.images.index(of: selectedImage) {
+                let selectedAsset = self.assets[index]
+                
+                let imageManager = PHImageManager.default()
+                let targetSize = CGSize(width: 600, height: 600)
+                imageManager.requestImage(for: selectedAsset, targetSize: targetSize, contentMode: .aspectFill, options: nil) { (image, info) in
+                    header.photoImageView.image = image
+                }
+            }
+        }
         return header
     }
     
---
+++ InstagramFirebase/InstagramFirebase/AddPhoto/PhotoSelectorHeader.swift
@@ -0,0 +1,31 @@
+//
+//  PhotoSelectorHeader.swift
+//  InstagramFirebase
+//
+//  Created by Christophe Bugnon on 10/09/2018.
+//  Copyright © 2018 Christophe Bugnon. All rights reserved.
+//
+
+import UIKit
+
+class PhotoSelectorHeader: UICollectionViewCell {
+    
+    let photoImageView: UIImageView = {
+        let iv = UIImageView()
+        iv.backgroundColor = .cyan
+        iv.contentMode = .scaleAspectFill
+        iv.clipsToBounds = true
+        return iv
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        addSubview(photoImageView)
+        photoImageView.fillSuperview()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
